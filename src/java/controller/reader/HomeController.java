293ff18fdfae318099142a5a3aeb1c7db7a22fcd
NgocNhat1087 <nhatngocvip@gmail.com>
2026-01-25 01:15:03 +0700

    basemodel

--- src/java/controller/reader/HomeController.java
+++ src/java/controller/reader/HomeController.java
@@ -24,5 +24,5 @@ public class HomeController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
     }
-
+///
 }
